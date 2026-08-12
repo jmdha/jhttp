1b7609403710abdeb865121c3e33bb7dced562ea
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2026-08-12 21:34:32 +0200

    feat: status code func

--- jhttp.h
+++ jhttp.h
@@ -11,7 +11,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-enum jhttp_status {
+typedef enum {
 	JHTTP_STATUS_CONTINUE                = 100, // RFC 9110, 15.2.1
 	JHTTP_STATUS_SWITCHINGPROTOCOLS      = 101, // RFC 9110, 15.2.2
 
@@ -28,7 +28,6 @@ enum jhttp_status {
 	JHTTP_STATUS_FOUND                   = 302, // RFC 9110, 15.4.3
 	JHTTP_STATUS_SEEOTHER                = 303, // RFC 9110, 15.4.4
 	JHTTP_STATUS_NOTMODIFIED             = 304, // RFC 9110, 15.4.5
-	JHTTP_STATUS_USEPROXY                = 305, // RFC 9110, 15.4.6
 	JHTTP_STATUS_TEMPORARYREDIRECT       = 307, // RFC 9110, 15.4.8
 	JHTTP_STATUS_PERMANENTREDIRECT       = 308, // RFC 9110, 15.4.9
 
@@ -59,7 +58,7 @@ enum jhttp_status {
 	JHTTP_STATUS_SERVICEUNAVAILABLE      = 503, // RFC 9110, 15.6.4
 	JHTTP_STATUS_GATEWAYTIMEOUT          = 504, // RFC 9110, 15.6.5
 	JHTTP_STATUS_HTTPVERSIONNOTSUPPORTED = 505, // RFC 9110, 15.6.6
-};
+} jhttp_status;
 
 struct jhttp_header {
 	char* key;
@@ -93,6 +92,55 @@ struct jhttp {
 	size_t                   conn_capacity;
 	struct jhttp_connection* conns;
 };
+
+static const char* jhttp_status_string(jhttp_status status) {
+	switch (status) {
+		case 100: return "100 Continue";
+		case 101: return "101 Switching Protocols";
+		case 200: return "200 OK";
+		case 201: return "201 Created";
+		case 202: return "202 Accepted";
+		case 203: return "203 Non-Authoritative Information";
+		case 204: return "204 No Content";
+		case 205: return "205 Reset Content";
+		case 206: return "206 Partial Content";
+		case 300: return "300 Multiple Choices";
+		case 301: return "301 Moved Permanently";
+		case 302: return "302 Found";
+		case 303: return "303 See Other";
+		case 304: return "304 Not Modified";
+		case 307: return "307 Temporary Redirect";
+		case 308: return "308 Permanent Redirect";
+		case 400: return "400 Bad Request";
+		case 401: return "401 Unauthorized";
+		case 402: return "402 Payment Required";
+		case 403: return "403 Forbidden";
+		case 404: return "404 Not Found";
+		case 405: return "405 Method Not Allowed";
+		case 406: return "406 Not Acceptable";
+		case 408: return "408 Request Timeout";
+		case 409: return "409 Conflict";
+		case 410: return "410 Gone";
+		case 411: return "411 Length Required";
+		case 412: return "412 Precondition Failed";
+		case 413: return "413 Content Too Large";
+		case 414: return "414 URI Too Long";
+		case 415: return "415 Unsupported Media Type";
+		case 416: return "416 Range Not Satisfiable";
+		case 417: return "417 Expectation Failed";
+		case 421: return "421 Misdirected Request";
+		case 422: return "422 Unprocessable Content";
+		case 426: return "426 Upgrade Required";
+		case 500: return "500 Internal Server Error";
+		case 501: return "501 Not Implemented";
+		case 502: return "502 Bad Gateway";
+		case 503: return "503 Service Unavailable";
+		case 504: return "504 Gateway Timeout";
+		case 505: return "505 HTTP Version Not Supported";
+		default: return "Unknown";
+	}
+
+}
 
 static int jhttp_request_parse(struct jhttp_request* req, char* str) {
 	struct jhttp_header* header = &req->headers[0];
